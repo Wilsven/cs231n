cecdcaad4fca5200b5c7b5aa3c63c57211ac3ff7
wilsven <wilsve@elevate-sg.com>
2022-12-09 22:09:39 +0800

    linear_svm.py

--- assignment1/cs231n/classifiers/linear_svm.py
+++ assignment1/cs231n/classifiers/linear_svm.py
@@ -37,6 +37,17 @@ def svm_loss_naive(W, X, y, reg):
             margin = scores[j] - correct_class_score + 1  # note delta = 1
             if margin > 0:
                 loss += margin
+                #########################################################
+                #                    START OF CHANGE                    #
+                #########################################################
+
+                dW[:, j] += X[i]    # update gradient for incorrect label
+                dW[:, y[i]] -= X[i] # update gradient for correct label
+
+                #########################################################
+                #                     END OF CHANGE                     #
+                #########################################################
+
 
     # Right now the loss is a sum over all training examples, but we want it
     # to be an average instead so we divide by num_train.
@@ -55,7 +66,8 @@ def svm_loss_naive(W, X, y, reg):
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    pass
+    dW /= num_train     # average over number of training examples
+    dW += 2 * reg * W   # add partial derivative of regularization term
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
@@ -78,7 +90,22 @@ def svm_loss_vectorized(W, X, y, reg):
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    pass
+    num_train = X.shape[0]
+
+    scores = X.dot(W)
+    true_scores = scores[range(num_train), y].reshape(-1, 1) # scores of true labels
+    # print(true_scores.shape)
+
+    margins = np.maximum(0, scores - true_scores + 1) 
+    margins[range(num_train), y] = 0
+    loss += margins.sum() # set margins of true labels to 0
+    
+    # Right now the loss is a sum over all training examples, but we want it
+    # to be an average instead so we divide by num_train.
+    loss /= num_train
+
+    # Add regularization to the loss.
+    loss += reg * np.sum(W * W)
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
@@ -93,8 +120,12 @@ def svm_loss_vectorized(W, X, y, reg):
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    pass
-
+    mask = (margins > 0).astype(int)
+    mask[range(num_train), y] -= mask.sum(axis=1)
+    dW = X.T.dot(mask)
+    dW /= num_train   # average over number of training examples
+    dW += 2 * reg * W # add partial derivative of regularization term
+    
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
     return loss, dW
